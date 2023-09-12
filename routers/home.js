ab05d53285059c5103f3ae8a6ee72378b4abff3f
Hendar <hendarsundana@gmail.com>
2023-09-12 21:51:30 +0700

    new

---
+++ routers/home.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const home = express.Router();
+const HomeController = require("../controllers/home.controller");
+
+const homeController = new HomeController();
+home.get("/", homeController.index);
+home.get("/2", (req, res) => {
+    res.send("Hello 2")
+});
+
+module.exports = home;
